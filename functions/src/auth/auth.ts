8cd2fb32411bb8980031d3f81388f4335780edc5
Harsha Srikara <harshasrikara@gmail.com>
2020-06-20 17:51:36 -0500

    several sample functions & deployed

--- functions/src/auth/auth.ts
+++ functions/src/auth/auth.ts
@@ -1,44 +1,27 @@
-import * as functions from "firebase-functions";
-import { auth, firestore } from "../admin/admin";
-const jwt = require("express-jwt");
-const jwksRsa = require("jwks-rsa");
 
-const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${functions.config().auth0.domain}/.well-known/jwks.json`,
-  }),
-
-  audience: functions.config().auth0.audience,
-  issuer: `https://${functions.config().auth0.domain}/`,
-  algorithm: ["RS256"],
-});
+import { auth } from "../admin/admin";
 
 /**
  * Receive the {getTokenSilently} from auth0
  */
-const createCustomToken = functions.https.onRequest(
-  (request: any, response) => {
-    const { sub: uid } = request.user;
+exports.createCustomToken = (request: any, response: any) => {
+  const { sub: uid } = request.user;
 
-    const firebaseToken = auth
-      .createCustomToken(uid)
-      .then((customToken) => {
-        response.json({ firebaseToken });
-      })
-      .catch(function (error) {
-        response.status(500).send({
-          message: "Something went wrong acquiring a Firebase token.",
-          error: error,
-        });
-        console.log("Error creating custom token:", error);
+  const firebaseToken = auth
+    .createCustomToken(uid)
+    .then((customToken) => {
+      response.json({ firebaseToken });
+    })
+    .catch(function (error) {
+      response.status(500).send({
+        message: "Something went wrong acquiring a Firebase token.",
+        error: error,
       });
-  }
-);
+      console.log("Error creating custom token:", error);
+    });
+};
 
-const createTestUser = functions.https.onRequest((request, response) => {
+exports.createTestUser = (request: any, response: any) => {
   auth
     .createUser({
       email: "user@example.com",
@@ -56,4 +39,4 @@ const createTestUser = functions.https.onRequest((request, response) => {
     .catch(function (error) {
       console.log("Error creating new user:", error);
     });
-});
+};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,39 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+const authFunctions = require('./auth/auth');
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const express = require("express");
+const cors = require("cors");
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+
+/**
+ * Only handle routes and connecting them to functions here
+ */
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${functions.config().auth0.domain}/.well-known/jwks.json`,
+  }),
+
+  audience: functions.config().auth0.audience,
+  issuer: `https://${functions.config().auth0.domain}/`,
+  algorithm: ["RS256"],
+});
+
+// Add middleware to authenticate requests
+
+app.get('/getCustomToken',checkJwt, authFunctions.createCustomToken);
+app.post('/createTestUser', authFunctions.createTestUser);
+app.get('/hello-world', (req: any, res: any) => {
+    res.send("hello world!");
+})
+
+exports.api = functions.https.onRequest(app);
