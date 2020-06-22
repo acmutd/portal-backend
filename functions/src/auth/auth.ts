49ba02ec3a9ae16d58662d84ded7ced688837692
Harsha Srikara <harshasrikara@gmail.com>
2020-06-21 23:58:18 -0500

    minor improvements

--- functions/src/auth/auth.ts
+++ functions/src/auth/auth.ts
@@ -21,6 +21,30 @@ exports.createCustomToken = (request: any, response: any) => {
     });
 };
 
+exports.createAuth0User = (request: any, response: any) => {
+  const { sub: uid } = request.user;
+
+  const firebaseToken = auth
+    .createCustomToken(uid)
+    .then((customToken) => {
+      console.log("logging firebase token");
+      console.log(firebaseToken);
+      console.log("logging custom token");
+      console.log(customToken);
+
+
+      auth.createUser
+      response.json({ firebaseToken });
+    })
+    .catch(function (error) {
+      response.status(500).send({
+        message: "Something went wrong acquiring a Firebase token.",
+        error: error,
+      });
+      console.log("Error creating custom token:", error);
+    });
+};
+
 exports.createTestUser = (request: any, response: any) => {
   auth
     .createUser({
@@ -35,8 +59,10 @@ exports.createTestUser = (request: any, response: any) => {
     .then(function (userRecord) {
       // See the UserRecord reference doc for the contents of userRecord.
       console.log("Successfully created new user:", userRecord.uid);
+      response.json(userRecord.toJSON());
     })
     .catch(function (error) {
       console.log("Error creating new user:", error);
+      response.send("Error creating new user");
     });
 };
