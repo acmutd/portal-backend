b68050764bdd7d077ec307b504279165e2f140e4
Aliah <asndeguzman@gmail.com>
2020-07-09 22:54:15 -0500

    changed functions to async

--- functions/src/roles/roles.ts
+++ functions/src/roles/roles.ts
@@ -5,72 +5,76 @@ const admin = require('firebase-admin');
  * Data object = ['docName string', 'permission string']
  */
 
-exports.createRole = (request: any, response: any) => {
+async function createRole(request: any, response: any) {
     const data = JSON.parse(request.body);
     
     const docName = data[0]
     const permission = data[1]
-    // Add a new document in collection "roles"
-    firestore
-      .collection("roles")
-      .doc(docName)
-      .set({
-          permissions: admin.firestore.FieldValue.arrayUnion(permission)   
-      })
-      .then(() => {
-        //success execution
-        response.json(data); //sends a json
-      })
-      .catch((error: any) => {
-        //failure execution
-        response.send("You broke it");
+    try{
+      await firestore.collection("roles").doc(docName).set({
+        permissions: admin.firestore.FieldValue.arrayUnion(permission) 
       });
+      response.json(data)
+    } catch(error){
+      response.json("you broke it")
+    }
   };
 
   /**
  * Data object = ['docName string', 'permission string']
  */
 
-  exports.updateRole = (request: any, response: any) => {
+async function updateRole(request: any, response: any)  {
     const data = JSON.parse(request.body);
     
 // updates a new document in collection "roles"
     const docName = data[0]
     const permission = data[1]
-    firestore
-      .collection("roles")
-      .doc(docName)
-      .update({
-          permissions: admin.firestore.FieldValue.arrayUnion(permission)   
-      })
-      .then(() => {
-        //success execution
-        response.json(data); //sends a json
-      })
-      .catch((error: any) => {
-        //failure execution
-        response.send("You broke it");
+    try{
+      await firestore.collection("roles").doc(docName).update({
+        permissions: admin.firestore.FieldValue.arrayUnion(permission) 
       });
+      response.json(data)
+    } catch(error){
+      response.json("you broke it")
+    }
   };
 
    /**
  * Data object = ['docName string', 'permission string']
  */
 
-  exports.deleteRole = (request: any, response: any) => {
-    const data = JSON.parse(request.body);
+async function deleteRole(request: any, response: any) {
+  const data = JSON.parse(request.body);
     
-    //deletes a certain document
-    firestore
-      .collection("divisions")
-      .doc(data.docName)
-      .delete()
-      .then(() => {
-        //success execution
-        response.json("document deleted"); //sends a json
-      })
-      .catch((error: any) => {
-        //failure execution
-        response.send("You broke it");
-      });
-  };
+  // deletes a certain document
+      const docName = data[0]
+      try{
+        await firestore.collection("roles").doc(docName).delete(); 
+        response.json(data)
+      } catch(error){
+        response.json("you broke it")
+      }
+};
+
+async function readRole(request: any, response: any) {
+  const data = JSON.parse(request.body);
+    
+  // deletes a certain document
+      const docName = data[0]
+      try{
+        const rolesRef = firestore.collection("roles").doc(docName); 
+        const doc = await rolesRef.get(); 
+        if(!doc.exists){
+          response.json("document does not exist")
+        } 
+        else{
+          response.json(doc.data()); 
+        }
+      } catch(error){
+        response.json("you broke it")
+      }
+};
+  
+
+  export { createRole, updateRole, deleteRole, readRole }; 
