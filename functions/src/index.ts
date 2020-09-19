a3775545f4c4d6b9da9e7650556f8c1d31c74bba
Harsha Srikara <harshasrikara@gmail.com>
2020-09-19 15:18:40 -0500

    auto-link generation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,6 +14,9 @@ import express from "express";
 import cors from "cors";
 import * as bodyParser from "body-parser";
 import { firestore } from "./admin/admin";
+import * as Sentry from "@sentry/node";
+import request from "request";
+import sendgrid from "@sendgrid/mail";
 
 import app from "./express";
 
@@ -170,3 +173,51 @@ app2.delete("/tags/:tag/:token", (request: any, response: any) => {
 });
 
 export const challenge = functions.https.onRequest(app2);
+
+// rebrandly / typeform / firestore / integration service
+// need to clean this up later
+
+export const create_vanity_link = functions.firestore
+  .document("vanity_link/{document_name}")
+  .onCreate((snap, context) => {
+    const document_value = snap.data();
+    try {
+      const linkRequest = {
+        destination: document_value.vanity_redirect,
+        domain: { fullName: document_value.vanity_domain + ".acmutd.co" },
+        slashtag: document_value.vanity_slash,
+      };
+
+      const requestHeaders = {
+        "Content-Type": "application/json",
+        apikey: functions.config().rebrandly.apikey,
+      };
+
+      request(
+        {
+          uri: "https://api.rebrandly.com/v1/links",
+          method: "POST",
+          body: JSON.stringify(linkRequest),
+          headers: requestHeaders,
+        },
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (err: any, res: any, body: string) => {
+          //const link = JSON.parse(body);
+          sendgrid.setApiKey(functions.config().sendgrid.apikey);
+          const msg: sendgrid.MailDataRequired = {
+            from: "development@acmutd.co",
+            to: document_value.email,
+            dynamicTemplateData: {
+              preheader: "Successful Generation of Vanity Link",
+              subject: "Vanity Link",
+              link: document_value.vanity_domain + ".acmutd.co/" + document_value.vanity_slash,
+            },
+            templateId: "d-cd15e958009a43b3b3a8d7352ee12c79",
+          };
+          sendgrid.send(msg);
+        }
+      );
+    } catch (error) {
+      Sentry.captureException(error);
+    }
+  });
