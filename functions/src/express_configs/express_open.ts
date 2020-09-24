8f406566f11416b705bffb90e56ca1b03227e4c6
Harsha Srikara <harshasrikara@gmail.com>
2020-09-23 21:32:38 -0500

    typeform endpoint complete, open/secure endpoints

--- functions/src/express_configs/express_open.ts
+++ functions/src/express_configs/express_open.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import cors from "cors";
 import * as bodyParser from "body-parser";
+import * as functions from "firebase-functions";
+import * as Sentry from "@sentry/node";
+
+if (functions.config()?.sentry?.dns) Sentry.init({ dsn: functions.config().sentry.dns });
 
 const app = express();
+app.use(Sentry.Handlers.requestHandler());
 app.use(cors({ origin: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,11 +13,8 @@ import * as applicationFunctions from "./application/rebrand";
 import * as challengeFunctions from "./challenge/challenge";
 import * as sendgridFunctions from "./mail/sendgrid";
 import * as eventFunctions from "./events/events";
-
-import { firestore } from "./admin/admin";
-import * as Sentry from "@sentry/node";
-import request from "request";
-import sendgrid from "@sendgrid/mail";
+import * as vanityFunctions from "./custom/vanity";
+import * as typeformFunctions from "./application/typeform";
 
 //this will match every call made to this api.
 app_secure.all("/", (request, response, next) => {
@@ -88,58 +85,21 @@ app_secure.post("/sendgrid/send2", sendgridFunctions.sendDynamicTemplate);
 app_secure.post("/sendgrid/upsertContact", sendgridFunctions.upsertContact);
 app_secure.post("/sendgrid/sendMailingList", sendgridFunctions.sendMailingList);
 
+/**
+ * Challenges for ACM Development
+ */
 app_open.post("/tags/:tag", challengeFunctions.createTag);
 app_open.get("/tags/:tag", challengeFunctions.getTag);
 app_open.patch("/tags/:tag/:token", challengeFunctions.patchTag);
 app_open.delete("/tags/:tag/:token", challengeFunctions.deleteTag);
 
+/**
+ * typeform webhook
+ */
+app_open.post("/typeform", typeformFunctions.typeform_webhook);
+
 export const api = functions.https.onRequest(app_secure);
 export const challenge = functions.https.onRequest(app_open);
 
-// rebrandly / typeform / firestore / integration service
-// need to clean this up later
-
-export const create_vanity_link = functions.firestore
-  .document("vanity_link/{document_name}")
-  .onCreate((snap, context) => {
-    const document_value = snap.data();
-    try {
-      const linkRequest = {
-        destination: document_value.vanity_redirect,
-        domain: { fullName: document_value.vanity_domain + ".acmutd.co" },
-        slashtag: document_value.vanity_slash,
-      };
-
-      const requestHeaders = {
-        "Content-Type": "application/json",
-        apikey: functions.config().rebrandly.apikey,
-      };
-
-      request(
-        {
-          uri: "https://api.rebrandly.com/v1/links",
-          method: "POST",
-          body: JSON.stringify(linkRequest),
-          headers: requestHeaders,
-        },
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        (err: any, res: any, body: string) => {
-          //const link = JSON.parse(body);
-          sendgrid.setApiKey(functions.config().sendgrid.apikey);
-          const msg: sendgrid.MailDataRequired = {
-            from: "development@acmutd.co",
-            to: document_value.email,
-            dynamicTemplateData: {
-              preheader: "Successful Generation of Vanity Link",
-              subject: "Vanity Link",
-              link: document_value.vanity_domain + ".acmutd.co/" + document_value.vanity_slash,
-            },
-            templateId: "d-cd15e958009a43b3b3a8d7352ee12c79",
-          };
-          sendgrid.send(msg);
-        }
-      );
-    } catch (error) {
-      Sentry.captureException(error);
-    }
-  });
+// firestore triggers
+export const create_vanity_link = vanityFunctions.create_vanity_link;
