4cdd169efadc9821141b8653301da59c23a3dd5c
EWizard512 <42073460+EWizard512@users.noreply.github.com>
2021-08-08 16:00:51 -0500

    **Notes** Built express route to retrieve event data from firebase. Pending testing to be done with new Event Component.

--- functions/src/routes/express_portal.ts
+++ functions/src/routes/express_portal.ts
@@ -29,6 +29,7 @@ app_portal.get("/gsuite/verify", portalFunctions.verify);
 app_portal.get("/auth0/create-blank-profile", portalFunctions.create_blank_profile);
 app_portal.get("/auth0/profile", portalFunctions.get_profile);
 app_portal.get("/auth0/developer", portalFunctions.get_developer_profile);
+app_portal.get("/auth0/event", portalFunctions.get_event);
 app_portal.get("/auth0/checkin", portalFunctions.record_event);
 app_portal.get("/auth0/applications", get_active_applications);
 app_portal.get("/auth0/discord", get_user_metadata);
