51604a2b3e81ae7a02c88e60a6cf5afc3632da5c
DedsecKnight <mike.nguyen@acmutd.co>
2021-10-04 12:22:47 -0500

    Added @return and type alias to request-validation logic

--- functions/src/services/validate-request.ts
+++ functions/src/services/validate-request.ts
@@ -3,14 +3,27 @@ import { NextFunction, Request, Response } from "express";
 
 /**
  *
- * Function to enforce a required field in request body
+ * Interface representing object containing parameters used by requireField
  *
  * @param fieldName - name of field that needs to be enforced as required
  * @param onErrorMsg - if this field is not included, an error object will be returned including the message stored in this variable
  *
+ */
+interface RequireFieldParams {
+  fieldName: string;
+  onErrorMsg: string;
+}
+
+/**
+ *
+ * Function to enforce a required field in request body
+ *
+ * Parameter documentation can be found above
+ *
+ * @return - a middleware function that will be used by express-validator to check whether given field is included in request body
  *
  */
-export const requireField = ({ fieldName, onErrorMsg }: { fieldName: string; onErrorMsg: string }) => {
+export const requireField = ({ fieldName, onErrorMsg }: RequireFieldParams) => {
   return body(fieldName).not().isEmpty().withMessage(onErrorMsg);
 };
 
@@ -20,6 +33,7 @@ export const requireField = ({ fieldName, onErrorMsg }: { fieldName: string; onE
  *
  * @param onErrorMsg - Upon validation failure, response will return an object with the message stored in this variable
  *
+ * @return - a middleware function that will return 400 response if validation fails
  *
  */
 export const validateRequest = ({ onErrorMsg }: { onErrorMsg: string }) => {
