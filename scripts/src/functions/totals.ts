e4345afc2418ff3209e788324518bc78c22daaec
legitatx <ryan.donaldson@simplify.jobs>
2021-10-27 23:19:09 -0500

    chore(scripts): Update console message + Doppler value for doc

--- scripts/src/functions/totals.ts
+++ scripts/src/functions/totals.ts
@@ -5,6 +5,7 @@ import { environment } from "../environment";
 const event_collection = environment.FIRESTORE_EVENT_COLLECTION as string;
 const totals_collection = environment.FIRESTORE_TOTALS_COLLECTION as string;
 const totals_doc = environment.FIRESTORE_TOTALS_DOC as string;
+const total_event_current_semester_doc = environment.FIRESTORE_TOTAL_EVENTS_DOC as string;
 
 export const computeCollectionTotals = async (): Promise<void> => {
   firestore.listCollections().then(async (collections) => {
@@ -37,7 +38,7 @@ export const fetchParticipantCount = async (): Promise<void> => {
     .listDocuments()
     .then(async (documents) => {
       const attendanceCountForDocument: Record<string, number> = {};
-      const eventTotalRef = firestore.collection(totals_collection).doc("event");
+      const eventTotalRef = firestore.collection(totals_collection).doc(total_event_current_semester_doc);
 
       for (const document of documents) {
         const documentRef = await document.get();
@@ -49,10 +50,10 @@ export const fetchParticipantCount = async (): Promise<void> => {
 
             try {
               await eventTotalRef.update(attendanceCountForDocument);
-              console.log(`Total attendance for event ${documentRef.id} is ${attendanceCount}`);
+              console.log(`Total checked-in event attendance for event ${documentRef.id} is ${attendanceCount}`);
             } catch (error) {
               Sentry.captureException(error);
-              console.log(`Failed to fetch total attendance for event ${documentRef.id}`);
+              console.log(`Failed to fetch total checked-in event attendance for event ${documentRef.id}`);
             }
           }
         }
